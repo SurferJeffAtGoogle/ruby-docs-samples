ea619d8172640967cfb5a099d3de259a83550198
Jeffrey Rennie <rennie@google.com>
2018-08-07 15:25:52 -0700

    list_ips is code complete.

--- stackdriver/uptime_check.rb
+++ stackdriver/uptime_check.rb
@@ -1,18 +1,37 @@
 # bundle exec ruby uptime_check.rb
 require "google/cloud/monitoring/v3"
 
-uptime_check_service_client = Google::Cloud::Monitoring::V3::UptimeCheck.new
+def list_ips
+  uptime_check_service_client = Google::Cloud::Monitoring::V3::UptimeCheck.new
 
-# Iterate over all results.
-uptime_check_service_client.list_uptime_check_ips.each do |element|
-  # Process element.
+  # Iterate over all results.
+  uptime_check_service_client.list_uptime_check_ips.each do |element|
+    puts "#{element.location} #{element.ip_address}"
+  end
 end
 
-# Or iterate over results one page at a time.
-uptime_check_service_client.list_uptime_check_ips.each_page do |page|
-  # Process each page at a time.
-  page.each do |element|
-    p element
-    # Process element.
+if __FILE__ == $PROGRAM_NAME
+  command    = ARGV.shift
+
+  case command
+  when "list_ips"
+    list_ips()
+  when "inspect_file"
+    inspect_file(
+      project_id: project_id,
+      filename: ARGV.shift.to_s,
+      max_findings: ARGV.shift.to_i
+    )
+  else
+    puts <<-usage
+Usage: ruby uptime_check.rb <command> [arguments]
+
+Commands:
+  list_ips <content> <max_findings> Lists the ip address of uptime check servers.
+  inspect_file <filename> <max_findings> Inspect a local file.
+
+Environment variables:
+  GOOGLE_APPLICATION_CREDENTIALS set to the path to your JSON credentials
+    usage
   end
 end
